68f35074ef82c6451195285e60bad3b538eefeb8
Akshat0102 <76106653+Akshat0102@users.noreply.github.com>
2022-08-08 10:45:46 +0530

    wrote the tests for the contract :clipboard:

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const ganache = require('ganache');
+const { interface, bytecode } = require('../compile');
 
 //Web3 is a constructor of web3 library and web3 is an instance created
 const Web3 = require('web3');
@@ -7,3 +8,39 @@ const Web3 = require('web3');
 //provider is a communication layer used to interact b/w web3 and ganache
 const web3 = new Web3(ganache.provider());
 
+let accounts, inbox;
+
+beforeEach(async () => {
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+
+    //Use one of the accounts to deploy the contract
+
+    //interface or ABI given by solc as one of the o/p have to be parsed by JSON to give javascript object
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode, //bytecode is the other o/p given by solc after compiling the contract
+            arguments: ["Gotcha"]
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+});
+
+describe('Inbox', () => {
+    it('contract deployed', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('contains def msg', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "Gotcha")
+    });
+
+    it('updates msg', async () => {
+        await inbox.methods.setMessage("Gotcha Again").send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "Gotcha Again");
+    })
+});
